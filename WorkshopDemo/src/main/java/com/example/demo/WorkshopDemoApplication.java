02ef704f675b5742dfc2c7283c06f674003d73a8
Matthias Haeussler <maeddes@gmail.com>
2019-01-09 14:21:34 +0100

    config props

--- WorkshopDemo/src/main/java/com/example/demo/WorkshopDemoApplication.java
+++ WorkshopDemo/src/main/java/com/example/demo/WorkshopDemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class WorkshopDemoApplication {
+	
+	@Value("${my.applications.name: Fallback}")
+	String env;
+	
+	@GetMapping("/test")
+	String getName() {
+		
+		return this.env;
+		
+	}
 	
 	@GetMapping("/hello")
 	String sayHello() {
